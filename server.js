f9ea732f75447c0a17cb81ecb467145526fb057a
muusyaeric <ericmuusya909@gmail.com>
2023-06-09 00:16:40 +0300

    changed frontend link in forgot-password route

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const PORT = process.env.PORT
 const cors = require('cors')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
+app.use(cors({ credentials: true, origin: 'https://password-reset-bu5f.onrender.com' }))
 app.use(express.json())
 
 const authRoute = require('./controllers/authController')
